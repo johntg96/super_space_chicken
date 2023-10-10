e330194f24016d7c0f90952ad24e8e261d644403
John Garrison <splendidonkey@gmail.com>
2023-10-10 15:36:09 -0500

    added GameScene constructor

--- main.js
+++ main.js
@@ -6,11 +6,31 @@ const sizes = {
   height: 500
 }
 
+const speedDown = 300;
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("scene-game")
+  }
+
+  preload(){}
+  create(){}
+  update(){}
+}
+
 const config = {
   type: Phaser.WEBGL,
   width: 500,
   height: 500,
-  canvas: gameCanvas
+  canvas: gameCanvas,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y:speedDown },
+      debug: true
+    }
+  },
+  scene: [gameScene]
 }
 
 const game = new Phaser.Game(config)
