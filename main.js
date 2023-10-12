6eb45f32e129f888e566246897e7f2abfe5cc04f
John Garrison <splendidonkey@gmail.com>
2023-10-12 11:33:14 -0500

    added player object with ssc image. Applied cursor control using up/down arrow keys for player movement. Added space background. Set player collision with world border.

--- main.js
+++ main.js
@@ -11,17 +11,41 @@ const speedDown = 300;
 class GameScene extends Phaser.Scene {
   constructor() {
     super("scene-game")
+    this.player
+    this.cursor
+    this.playerSpeed = speedDown + 50
   }
 
-  preload(){}
-  create(){}
-  update(){}
+  preload(){
+    this.load.image("bg", "/assets/space_bg.jpg")
+    this.load.image("super_space_chicken", "/assets/super_space_chicken_smallest.png")
+  }
+  create(){
+    this.add.image(0, 0, "bg").setOrigin(0, 0)
+    this.player = this.physics.add.image(20, sizes.height / 2, "super_space_chicken").setOrigin(0, 0)
+    this.player.setImmovable(true)
+    this.player.body.allowGravity = false
+    this.player.setCollideWorldBounds(true)
+
+    this.cursor = this.input.keyboard.createCursorKeys()
+  }
+  update(){
+    const {up, down} = this.cursor;
+
+    if (up.isDown) {
+      this.player.setVelocityY(-this.playerSpeed);
+    } else if (down.isDown) {
+      this.player.setVelocityY(this.playerSpeed);
+    } else {
+      this.player.setVelocityY(0);
+    }
+  }
 }
 
 const config = {
   type: Phaser.WEBGL,
-  width: 500,
-  height: 500,
+  width: 1280,
+  height: 720,
   canvas: gameCanvas,
   physics: {
     default: "arcade",
@@ -30,7 +54,7 @@ const config = {
       debug: true
     }
   },
-  scene: [gameScene]
+  scene: [GameScene]
 }
 
 const game = new Phaser.Game(config)
